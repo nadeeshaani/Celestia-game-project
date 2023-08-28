a58f725b0775e0a5c978df2a0627915e23fb4439
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-08-28 11:08:55 +0530

    added button behaviour

--- Assets/Scripts/PlanetSkyBox.cs
+++ Assets/Scripts/PlanetSkyBox.cs
@@ -6,12 +6,16 @@ public class PlanetSkyBox : MonoBehaviour
 {
     public Material planetSkybox;  // Assign the respective planet's skybox material
     public GameObject objectsToHide; // Assign the parent GameObject for objects you want to hide
+    public ShowButtonOnPlanetClick buttonScript;  // Assign the ShowButtonOnPlanetClick script component
 
 
     private void OnMouseDown()
     {
         // Hide the current objects
         objectsToHide.SetActive(false);
+
+        // Show the button
+        buttonScript.ShowButton();
 
         // Set the new skybox
         RenderSettings.skybox = planetSkybox;
---
+++ Assets/Scripts/ShowButtonOnPlanetClick.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowButtonOnPlanetClick : MonoBehaviour
+{
+    public GameObject button;  // Assign the Button GameObject in the Inspector
+
+
+    private void Start()
+    {
+        button.SetActive(false);  // Hide the button initially
+    }
+
+    public void ShowButton()
+    {
+        button.SetActive(true);  // Show the button
+    }
+}
