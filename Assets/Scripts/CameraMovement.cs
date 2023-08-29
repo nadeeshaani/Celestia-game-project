609ada1ed261b41f4ccc63ba0d2a2f717363524c
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-08-29 14:36:44 +0530

    added keyboard navigations

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float sensitivity = 2.0f;  // Mouse sensitivity
+    public float mouseSensitivity = 2.0f;  // Mouse sensitivity
+    public float keyboardMoveSpeed = 5.0f;  // Keyboard movement speed
+
     private Vector3 rotation;
-
-
 
     void Update()
     {
-        // Rotate the camera based on mouse input
-        rotation.y += Input.GetAxis("Mouse X") * sensitivity;
-        rotation.x -= Input.GetAxis("Mouse Y") * sensitivity;
+        // Mouse rotation
+        rotation.y += Input.GetAxis("Mouse X") * mouseSensitivity;
+        rotation.x -= Input.GetAxis("Mouse Y") * mouseSensitivity;
         rotation.x = Mathf.Clamp(rotation.x, -90f, 90f); // Limit vertical rotation
         transform.eulerAngles = rotation;
+
+        // Keyboard movement
+        float horizontalInput = Input.GetAxis("Horizontal");  // Get horizontal arrow key input
+        float verticalInput = Input.GetAxis("Vertical");  // Get vertical arrow key input
+
+        // Calculate the movement direction
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput);
+
+        // Normalize the movement direction and apply the speed
+        Vector3 moveAmount = moveDirection.normalized * keyboardMoveSpeed * Time.deltaTime;
+
+        // Convert the local movement to global movement
+        Vector3 moveAmountGlobal = transform.TransformDirection(moveAmount);
+
+        // Apply the movement to the camera's position
+        transform.position += moveAmountGlobal;
     }
 }
