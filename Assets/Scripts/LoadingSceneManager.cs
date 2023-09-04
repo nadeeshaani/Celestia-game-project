893ffee84a3eeb6dcfcf6b21d5e0490d65e873f1
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-04 13:14:05 +0530

    loading screen added

---
+++ Assets/Scripts/LoadingSceneManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneManager : MonoBehaviour
+{
+    public void LoadSolarSystemScene()
+    {
+        SceneManager.LoadScene("SolarSystem"); // Replace with the actual scene name
+    }
+}
