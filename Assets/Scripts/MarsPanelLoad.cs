b2c5d1bf502e698a1979e50d777951ff5169e0af
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-12-05 21:15:18 +0530

    Planet details UIs added

---
+++ Assets/Scripts/MarsPanelLoad.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class MarsPanelLoad : MonoBehaviour
+{
+    public GameObject marsPanel; // Assign the UI panel for Earth in the Inspector
+    //public GameObject marsPanel; // Assign the UI panel for Mars in the Inspector
+    // Add more panels for other planets as needed...
+
+    void Start()
+    {
+        // Deactivate all panels when the scene starts
+        DeactivateAllPanels();
+    }
+
+    void DeactivateAllPanels()
+    {
+        marsPanel.SetActive(false);
+        // marsPanel.SetActive(false);
+        // Deactivate other panels for additional planets...
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        string collidedObjectName = collision.gameObject.name;
+
+        if (collidedObjectName.Equals("Mars"))
+        {
+            ActivatePanel(marsPanel);
+        }
+
+        //else if (collidedObjectName.Equals("Mars"))
+        //{
+        //    ActivatePanel(marsPanel);
+        //}
+        // Add more conditions for other planets...
+    }
+
+    void ActivatePanel(GameObject panel)
+    {
+        DeactivateAllPanels(); // Deactivate all panels first
+        panel.SetActive(true); // Activate the selected panel
+    }
+}
