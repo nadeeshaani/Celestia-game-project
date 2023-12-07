8d9f9d941c25937fd813981d9094158ee8651bb4
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-12-07 15:16:19 +0530

    updated

--- Assets/Scripts/GotoManual.cs
+++ Assets/Scripts/GotoManual.cs
@@ -3,8 +3,38 @@ using UnityEngine.SceneManagement;
 
 public class GotoManual : MonoBehaviour
 {
+    public AudioClip clickSound; // Drag and assign the click sound in the Inspector
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        // Fetch AudioSource component or add it if not present
+        audioSource = GetComponent<AudioSource>();
+        
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        // Set the click sound to the AudioSource
+        if (clickSound != null)
+        {
+            audioSource.clip = clickSound;
+        }
+    }
+
     public void LoadManual()
     {
-        SceneManager.LoadScene("HowToPlay"); // Replace with the actual scene name
+        // Play the click sound
+        if (audioSource != null && audioSource.clip != null)
+        {
+            audioSource.gameObject.SetActive(true);
+            audioSource.Play();
+        }
+
+        // Load the appropriate scene after a delay (if needed)
+        SceneManager.LoadScene("HowToPlay"); // Replace with your main menu scene name
     }
 }
+
