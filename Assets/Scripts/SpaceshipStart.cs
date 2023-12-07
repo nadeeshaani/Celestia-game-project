793ce333b890b32fd0a7916e4ef50b899a7a7e55
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-12-07 20:25:50 +0530

    updated

--- Assets/Scripts/SpaceshipStart.cs
+++ Assets/Scripts/SpaceshipStart.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class SpaceshipStart : MonoBehaviour
 {
+    [SerializeField] private Vector3 positionOffset = new Vector3(0.5f, 0.5f, 0); // Offset values (adjust as needed)
+
     // Start is called before the first frame update
     void Start()
     {
         DataManager dataManager = DataManager.GetInstance();
         if (dataManager != null)
         {
-            // Set the spaceship's position to the stored position
-            transform.position = dataManager.GetSpaceshipPosition();
+            // Set the spaceship's position to the stored position with the offset
+            Vector3 storedPosition = dataManager.GetSpaceshipPosition() + positionOffset;
+            transform.position = storedPosition;
+            dataManager.UpdateSpaceshipPosition(storedPosition); // Update DataManager with the new position
         }
     }
-
-   
 }
+
