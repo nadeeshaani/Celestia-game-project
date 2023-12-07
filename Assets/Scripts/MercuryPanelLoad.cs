a82580605b56b1d41c48f847613e46c36dd4758c
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-12-07 13:58:07 +0530

    updated

--- Assets/Scripts/MercuryPanelLoad.cs
+++ Assets/Scripts/MercuryPanelLoad.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
+using TMPro;
+
 
 public class MercuryPanelLoad : MonoBehaviour
 {
-    public GameObject mercuryPanel; // Assign the UI panel for Earth in the Inspector
-    //public GameObject marsPanel; // Assign the UI panel for Mars in the Inspector
-    // Add more panels for other planets as needed...
+
+    public TextMeshProUGUI landingText;
+    public TextMeshProUGUI landingInstruction;
+    public GameObject mercuryPanel;
+
+    public AudioSource notificationSound;
+
 
     void Start()
     {
         // Deactivate all panels when the scene starts
         DeactivateAllPanels();
+        landingText.gameObject.SetActive(false);
+        landingInstruction.gameObject.SetActive(true);
+        notificationSound.gameObject.SetActive(false);
     }
 
     void DeactivateAllPanels()
@@ -19,19 +28,22 @@ public class MercuryPanelLoad : MonoBehaviour
         // Deactivate other panels for additional planets...
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider other)
     {
-        string collidedObjectName = collision.gameObject.name;
+        string collidedObjectName = other.gameObject.name;
 
         if (collidedObjectName.Equals("Mercury"))
         {
             ActivatePanel(mercuryPanel);
+            ActivateLandingMessage();
+            DeactivateLandingInstruction();
+            PlayNotificationSound();
         }
 
-        //else if (collidedObjectName.Equals("Mars"))
-        //{
+        // else if (collidedObjectName.Equals("Mars"))
+        // {
         //    ActivatePanel(marsPanel);
-        //}
+        // }
         // Add more conditions for other planets...
     }
 
@@ -39,5 +51,30 @@ public class MercuryPanelLoad : MonoBehaviour
     {
         DeactivateAllPanels(); // Deactivate all panels first
         panel.SetActive(true); // Activate the selected panel
+    }
+
+    void ActivateLandingMessage()
+    {
+        landingText.gameObject.SetActive(true); // Activate the landing message
+        Invoke("DeactivateLandingMessage", 5.0f); // Automatically deactivate after 5 seconds
+    }
+
+    void DeactivateLandingMessage()
+    {
+        landingText.gameObject.SetActive(false); // Deactivate the landing message
+    }
+
+    void DeactivateLandingInstruction()
+    {
+        landingInstruction.gameObject.SetActive(false);
+    }
+
+    void PlayNotificationSound()
+    {
+        if (notificationSound != null)
+        {
+            notificationSound.gameObject.SetActive(true);
+            notificationSound.Play(); // Play the notification sound
+        }
     }
 }
