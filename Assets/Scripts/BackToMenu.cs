e2f08abcd533c162b22262986502f8884210b6fc
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-12-07 11:04:09 +0530

    updated

---
+++ Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    public void loadMenu()
+    {
+        SceneManager.LoadScene("LoadingScene"); // Replace with the actual scene name
+    }
+}
---
+++ Assets/Scripts/GotoManual.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GotoManual : MonoBehaviour
+{
+    public void LoadManual()
+    {
+        SceneManager.LoadScene("HowToPlay"); // Replace with the actual scene name
+    }
+}
